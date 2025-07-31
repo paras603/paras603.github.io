a435c6835b5fe0b4394492be14f20a2cd89fa026
paras603 <parasmanirai1@gmail.com>
2025-07-30 22:32:08 -0400

    trying to deploy

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/paras603.github.io/',
   plugins: [
     react(),
     tailwindcss(),
